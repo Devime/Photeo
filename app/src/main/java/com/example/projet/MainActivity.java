b1cdd7efbfecc390c4efe6d4fd55d13dbcb301dd
Devime <blain.dav@gmail.com>
2020-11-22 13:39:18 +0400

    try apn+save

--- app/src/main/java/com/example/projet/MainActivity.java
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        View.OnClickListener ltoapn = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Apnperformer.class);
+                startActivity(intent);
+                ActivityCompat.finishAffinity(MainActivity.this);
+            }
+        };
+
+        View.OnClickListener lgal = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(this,todo.class);
+            }
+        };
+
+        View.OnClickListener lexit = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(-1);
+            }
+        };
+
+
+        Button toapn = findViewById(R.id.Appareil);
+        Button gal = findViewById(R.id.Galerie);
+        Button exit = findViewById(R.id.Exit);
+
+        toapn.setOnClickListener(ltoapn);
+        gal.setOnClickListener(lgal);
+        exit.setOnClickListener(lexit);
+
     }
+
+
+
+
 }
